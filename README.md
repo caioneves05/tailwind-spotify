f2c1f96e8d3094a205c1e4c3b6ca1cf0aedbffc4
caioneves05 <ncaio037@gmail.com>
2024-01-20 21:17:25 -0300

    feat: add README

---
+++ README.md
@@ -0,0 +1,10 @@
+# 🎧🟢 Tailwind Spotify
+
+
+Explorando o comportamento e a flexibilidade ao utilizar o Tailwind CSS, em conjunto com a poderosa integração do Typescript.
+
+Este projeto visa exclusivamente o estudo e aprimoramento de conceitos no desenvolvimento front-end, seguindo como referência o modelo do Spotify Desktop.
+É relevante destacar que este projeto é um exemplo de baixa fidelidade, contendo apenas elementos estáticos e desprovido de funcionalidades, servindo apenas como um exercício de estilização.
+
+
+![teça](./src/app/assets/spotify-print.png)
--- src/app/components/molecules/SuggestionPlaylist/mocks/mock.ts
+++ src/app/components/molecules/SuggestionPlaylist/mocks/mock.ts
@@ -4,10 +4,6 @@ export const playlistsMock = [
         name: 'Esquenta Sertanejo'
     },
     {
-        avatar: 'https://scontent-gru2-2.xx.fbcdn.net/v/t1.18169-9/18198477_1119387444832353_9168490514684130984_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=9267fe&_nc_ohc=v_liCsXYiy8AX_tlTxw&_nc_ht=scontent-gru2-2.xx&oh=00_AfANW-z2gvaATy3JzyTKvDFkT0Jvl58g5g_d-3G9bBKRig&oe=652E8C21',
-        name: 'Brasil 360'
-    },
-    {
         avatar: 'https://bebe.abril.com.br/wp-content/uploads/2019/10/top-brasil.jpg?quality=90&strip=info&resize=1200,800',
         name: 'Top Brasil'
     },
